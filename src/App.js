76b002473c821e6467c03ccbe831e2440f2bb7c6
Adnan <adbak.dev@gmail.com>
2022-11-21 09:54:04 +0100

    added first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import styled from "styled-components";
+import profilePic from "./profile.png";
+
+import StyledComponent from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 200px;
+  color: #444;
+  border: 3px solid #1890ff;
+  background: #1890ff;
+  border-radius: 10px;
+  margin: auto;
+  margin-top: 20px;
+  padding-bottom: 10px;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  border-radius: 10px;
+  padding-top: 0;
+  margin-top: 0;
+`;
+
+const Title = styled.h1`
+  color: white;
+  font-weight: 400;
+  background: #1890ff;
+  margin: 0;
+  padding: 0;
+`;
+
+const Bio = styled.p`
+  color: white;
+  text-align: center;
+`;
+
+const App = () => (
+  <Container>
+    <Img src={profilePic} />
+    <Title>Bob Bobson</Title>
+    <Bio>I'm that Bob that you see all the time in your quests </Bio>
+  </Container>
+);
 
 export default App;
